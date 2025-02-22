fc00285d6d399ddd6237811eaee87709fbd5ecdf
Suvam Tripathy <131255895+SuvamT0071@users.noreply.github.com>
2025-02-22 21:00:44 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='isingmetro',
     version='0.1.0',
-    packages=find_packages(), 
+    packages = ['isingmetro'], 
     install_requires=['numpy', 'matplotlib', 'tqdm'],
     author='SuvamT0071',
     author_email='profsuvam@gmail.com',
